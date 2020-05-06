4d23360d4f301d784adf3bcc4fdc700deeb5f321
mattmoniz <Monizri@gmail.com>
2020-05-06 18:45:31 -0400

    added img_url column to books table and also to the serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,5 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :description, :isbn
+  attributes :id, :title, :author, :description, :isbn, :img_url
 
 has_many :libraries
 end
