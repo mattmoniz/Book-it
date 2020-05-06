9fd94be338de7dc23858e8a963aefc4eac6d5d31
mattmoniz <Monizri@gmail.com>
2020-05-06 17:15:23 -0400

    added extra functionality to get extra data end points from google books - NOT COMPLETE

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,18 +2,43 @@ class Api::V1::BooksController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
+    title=[]
     image_urls = []
+    authors=[]
+    description=[]
+    isbn=[]
+
     base_url = "https://www.googleapis.com/books/v1/volumes"
     num = "40"
-    query = "Android"
+    query = "llama llama Red Pajama"
     response = Faraday.get("#{base_url}?&maxResults=#{num}&q=#{query}&key=#{ENV["GOOGLE_BOOKS_API_KEY"]}")
     parsed_response = JSON.parse(response.body)
 
     parsed_response["items"].each do |book|
+
       if book["volumeInfo"]["imageLinks"].present?
         image_urls << book["volumeInfo"]["imageLinks"]["thumbnail"]
       end
+
+      title << book["volumeInfo"]["title"]
+      authors << book["volumeInfo"]["authors"][0]
+      description << book["volumeInfo"]["description"]
+
+      book["volumeInfo"]["industryIdentifiers"].each do |isbnID|
+        if isbnID["type"] == "ISBN_13"
+          isbn << isbnID["identifier"]
+        end
+      end
+
     end
-    render json: image_urls
+
+  render json: {
+      title: title,
+      authors: authors,
+      description: description,
+      image_urls: image_urls,
+      isbn: isbn
+    }
+
   end
 end
--- app/javascript/react/containers/BooksIndexContainer.js
+++ app/javascript/react/containers/BooksIndexContainer.js
@@ -3,33 +3,43 @@ import BookTile from "../components/BookTile";
 
 const BooksIndexContainer = (props) => {
   const [books, setBooks] = useState([])
+ // debugger
+const fetchBookData = () => {
+  fetch("/api/v1/books")
+    .then(response => response.json())
+    .then(booksBody => {
+      debugger
+      setBooks(booksBody)
+        debugger
+    }
+  )
+}
+
   useEffect(() => {
-    fetch("/api/v1/books")
-      .then(response => response.json())
-      .then(booksBody => {
-        setBooks(booksBody)
-      })
+    fetchBookData();
   }, [])
 
-  const bookImages = books.map((bookLink) => {
+  const bookInfo = books.map((bookData) => {
+    debugger
     return(
       <BookTile
-        key={bookLink.id}
-        book={bookLink}
+        key={bookData.id}
+        title={title}
+        book={bookData.image_urls}
         />
     )
   })
   return (
     <div className="center">
       <form>
-        <div class="form-group">
+        <div className="form-group">
           <input type="text" className="form-control mt-10" placeholder="Search for New Books"/>
         </div>
         <button type="submit" className="btn btn-danger">Search</button>
       </form>
 
       <h1>THE Best Books</h1>
-        {bookImages}
+        {bookInfo}
 
     </div>
   )
