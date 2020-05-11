67160b85bd55c8644c64a3e3c0a0552dacdfc8f6
mattmoniz <Monizri@gmail.com>
2020-05-11 09:13:39 -0400

    added button to showtile

--- app/javascript/react/components/BookShowTIle.js
+++ app/javascript/react/components/BookShowTIle.js
@@ -1,17 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const BookShowTile = (props) => {
-
-  const [bookRecord, setAddbookRecord] = useState({});
-
+debugger
   return (
     <div className="book-tile callout">
         <img src={props.bookCover}  height="200" width="200"></img>
           <p> Title: {props.title}</p>
           <p> Authors: {props.authors}</p>
           <p> Description: {props.description}</p>
-          <input className="button center" type="submit" value="Add to Bookshelf"/>
   </div>
   );
 };
