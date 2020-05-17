c81851c19ad6bc3600ac524f20142528f3f93ca8
mattmoniz <Monizri@gmail.com>
2020-05-17 19:19:45 -0400

    sample color changes, added hover effect to buttons

--- app/javascript/react/components/BookTile.js
+++ app/javascript/react/components/BookTile.js
@@ -6,7 +6,7 @@ const BookTile = props => {
     <div className="callout book-tile">
 
       <Link to={`/books/${props.id}`}>
-        <img src={props.bookCover} height="300" width="300"></img>
+        <img className=" book-tile-image" src={props.bookCover} height="300" width="300"></img>
         <p className="booktile" > <b>Title: </b>{props.title}</p>
         <p className="booktile"> <b>Authors: </b>{props.authors}</p>
         <p className="booktile"> <b>Published Date: </b> {props.publishedDate}</p>
