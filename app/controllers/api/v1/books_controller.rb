3513a2e24f351fd3c6840a6dba1dbc4bf7ca002e
mattmoniz <Monizri@gmail.com>
2020-05-17 05:05:33 -0400

    callout to have devise render at when user arrives at the site.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BooksController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
